2b4a35cac2fc23fb848c2234ad9f85c8a540018a
ievans3024 <ievans3024@gmail.com>
2017-02-11 12:58:24 -0800

    rearranged declarations, made collection_json classes actually work, added module.exports

--- collection_json.js
+++ collection_json.js
@@ -1,144 +1,16 @@
 'use strict';
 
-class CollectionObject {
-
-  constructor (data) {
-
-    let property;
-
-    if (typeof data === 'string') {
-      data = JSON.parse(data);
-    }
-
-    // validate contents
-    data = this.validate(data);
-
-    // set properties from data
-    for (property in data) {
-      if (data.hasOwnProperty(property)) {
-        this[property] = data[property];
-      }
-    }
-
-  }
-
-  validate (object) {
-    let collection = {},
-        r,
-        rule;
-
-    for (rule in this.property_rules) {
-      if (this.property_rules.hasOwnProperty(rule)) {
-        r = this.property_rules[rule];
-        if (r.hasOwnProperty('required') && !object.hasOwnProperty(rule)) {
-          throw new Error('Missing required property: ' + rule);
-        } else if (object.hasOwnProperty(rule)) {
-          if (r.hasOwnProperty('type')) {
-            if (r.type === Array) {
-              collection[rule] = object[rule].map(
-                function (value) {
-                  if (r.hasOwnProperty('contents')) {
-                    if (!(value instanceof r.contents)) {
-                      return new r.contents(value);
-                    } else {
-                      return value;
-                    }
-                  } else {
-                    return value;
-                  }
-                }
-              )
-            } else if (!object[rule] instanceof r.type) {
-              collection[rule] = new r.type(object[rule]);
-            } else {
-              collection[rule] = object[rule];
-            }
-          }
-        }
-      }
-    }
-
-    return collection;
-
-  }
-
-  toString () {
-    return JSON.stringify(this);
-  }
-
-}
-
-class HasLinks {
-
-  get_link (href) {
-    return this.data.reduce(
-      function (prev, next) {
-        if (prev !== null) {
-          return prev;
-        } else if (next.href === href) {
-          return next;
-        } else {
-          return null;
-        }
-      },
-      null
-    );
-  }
-
-}
-
-class HasData {
-
-  get_data (name) {
-    return this.data.reduce(
-      function (prev, next) {
-        if (prev !== null) {
-          return prev;
-        } else if (next.name === name) {
-          return next;
-        } else {
-          return null;
-        }
-      },
-      null
-    );
-  }
-
-}
-
-class Collection extends CollectionObject, HasLinks {
-
-  get property_rules () {
-
-    return {
-      error: {type: CollectionError},
-      href: {type: String, required: true},
-      items: {type: ItemArray, contents: Item},
-      links: {type: LinkArray, contents: Link},
-      queries: {type: QueryArray, contents: Query},
-      template: {type: Template},
-      version: {type: String, required: true}
-    };
-
-  }
-
-  toString () {
-    return JSON.stringify({collection: this});
-  }
-
-}
-
 class CollectionArray extends Array {
 
-  constructor () {
-    let contains = arguments.slice(0,1),
-        super_args = arguments.slice(1);
+  constructor (...args) {
+    let contains = args.slice(0,1)[0],
+      super_args = args.slice(1);
+    super(...super_args);
     if (typeof contains !== 'function') {
       throw new Error('Argument "contains" must be a function.');
     } else {
       this.contains = contains;
     }
-    super(...super_args);
   }
 
   validate_content (object) {
@@ -161,30 +33,166 @@ class CollectionArray extends Array {
 
   splice () {
     let items = arguments.slice(2).map(this.validate_content),
-        args = arguments.slice(0,2) + items;
+      args = arguments.slice(0,2) + items;
     return super.splice(...args);
   }
 
 }
 
-class DataArray extends CollectionArray {
+class CollectionObject {
 
-  constructor () {
+  constructor (data) {
 
-    super(Data, ...arguments);
+    let property;
+
+    if (typeof data === 'string') {
+      data = JSON.parse(data);
+    }
+
+    // validate contents
+    data = this.validate(data);
+
+    // set properties from data
+    for (property in data) {
+      if (data.hasOwnProperty(property)) {
+        this[property] = data[property];
+      }
+    }
+
+    // add properties that were not set before as new empty properties
+    for (property in this.property_rules) {
+      if (this.property_rules.hasOwnProperty(property) && !this.hasOwnProperty(property)) {
+        this[property] = new this.property_rules[property]['type']();
+      }
+    }
 
   }
 
-}
+  validate (object) {
+    let collection = {},
+        r,
+        rule;
 
-class Data extends CollectionObject {
+    for (rule in this.property_rules) {
+      if (this.property_rules.hasOwnProperty(rule)) {
+        r = this.property_rules[rule];
+        if (r.hasOwnProperty('required') && (object === undefined || !object.hasOwnProperty(rule))) {
+          throw new Error('Missing required property: ' + rule);
+        } else if (object !== undefined){
+          if(object.hasOwnProperty(rule)) {
+            if (r.hasOwnProperty('type')) {
+              if (r.type === Array) {
+                collection[rule] = object[rule].map(
+                  function (value) {
+                    if (r.hasOwnProperty('contents')) {
+                      if (!(value instanceof r.contents)) {
+                        return new r.contents(value);
+                      } else {
+                        return value;
+                      }
+                    } else {
+                      return value;
+                    }
+                  }
+                )
+              } else if (!object[rule] instanceof r.type) {
+                collection[rule] = new r.type(object[rule]);
+              } else {
+                collection[rule] = object[rule];
+              }
+            }
+          }
+        }
+      }
+    }
 
-  get property_rules () {
-    return {
-      name: {type: String, required: true},
-      prompt: {type: String},
-      value: {type: String}
-    };
+    return collection;
+
+  }
+
+  toString () {
+    return JSON.stringify(this);
+  }
+
+  get_data (name) {
+
+    if (this.hasOwnProperty('data')) {
+
+      return this.get_data_from_array(this.data, 'name', name);
+
+    } else {
+
+      return null;
+
+    }
+
+  }
+
+  get_data_from_array (array, identifier, value) {
+
+    return array.reduce(
+      function (prev, next) {
+
+        if (prev !== null) {
+
+          return prev;
+
+        } else if (next[identifier] === value) {
+
+          return next;
+
+        } else {
+
+          return null;
+
+        }
+
+      },
+      null
+    );
+
+  }
+
+  get_item (href) {
+
+    if (this.hasOwnProperty('items')) {
+
+      return this.get_data_from_array(this.items, 'href', href);
+
+    } else {
+
+      return null;
+
+    }
+
+  }
+
+  get_link (href) {
+
+    if (this.hasOwnProperty('links')) {
+
+      return this.get_data_from_array(this.links, 'href', href);
+
+    } else {
+
+      return null;
+
+    }
+
+  }
+
+  get_query (name) {
+
+    if (this.hasOwnProperty('queries')) {
+
+      return this.get_data_from_array(this.queries, 'name', name);
+
+    } else {
+
+      return null;
+
+    }
+
   }
 
 }
@@ -198,31 +206,19 @@ class CollectionError extends CollectionObject {
       code: {type: String},
       message: {type: String}
     };
-    
+
   }
 
 }
 
-class Item extends CollectionObject, HasData, HasLinks {
+class Data extends CollectionObject {
 
   get property_rules () {
-
     return {
-      href: {type: String, required: true},
-      data: {type: DataArray},
-      links: {type: LinkArray}
+      name: {type: String, required: true},
+      prompt: {type: String},
+      value: {type: String}
     };
-
-  }
-
-}
-
-class ItemArray extends CollectionArray {
-
-  constructor () {
-
-    super(Item, ...arguments);
-
   }
 
 }
@@ -243,14 +239,54 @@ class Link extends CollectionObject {
 
 }
 
-class LinkArray extends CollectionArray {
+class DataArray extends CollectionArray {
 
-  constructor () {
+  constructor (...args) {
 
-    super(Link, ...arguments);
+    args.unshift(Data);
+
+    super(...args);
 
   }
-  
+
+}
+
+class LinkArray extends CollectionArray {
+
+  constructor (...args) {
+
+    args.unshift(Link);
+
+    super(...args);
+
+  }
+
+}
+
+class Item extends CollectionObject {
+
+  get property_rules () {
+
+    return {
+      href: {type: String, required: true},
+      data: {type: DataArray},
+      links: {type: LinkArray}
+    };
+
+  }
+
+}
+
+class ItemArray extends CollectionArray {
+
+  constructor (...args) {
+
+    args.unshift(Item);
+
+    super(...args);
+
+  }
+
 }
 
 class Query extends CollectionObject {
@@ -271,9 +307,11 @@ class Query extends CollectionObject {
 
 class QueryArray extends CollectionArray {
 
-  constructor () {
+  constructor (...args) {
 
-    super(Query, ...arguments);
+    args.unshift(Query);
+
+    super(...args);
 
   }
 
@@ -284,9 +322,47 @@ class Template extends CollectionObject {
   get property_rules () {
 
     return {
-        data: {type: DataArray}
+      data: {type: DataArray}
     };
 
   }
 
 }
+
+class Collection extends CollectionObject {
+
+  get property_rules () {
+
+    return {
+      error: {type: CollectionError},
+      href: {type: String, required: true},
+      items: {type: ItemArray, contents: Item},
+      links: {type: LinkArray, contents: Link},
+      queries: {type: QueryArray, contents: Query},
+      template: {type: Template},
+      version: {type: String, required: true}
+    };
+
+  }
+
+  toString () {
+    return JSON.stringify({collection: this});
+  }
+
+}
+
+module.exports = {
+  'Collection': Collection,
+  'CollectionArray': CollectionArray,
+  'CollectionError': CollectionError,
+  'CollectionObject': CollectionObject,
+  'Data': Data,
+  'DataArray': DataArray,
+  'Item': Item,
+  'ItemArray': ItemArray,
+  'Link': Link,
+  'LinkArray': LinkArray,
+  'Query': Query,
+  'QueryArray': QueryArray,
+  'Template': Template
+};
